9dbba5dfd59e8e740bbe77bc7917bede24796b98
Luke Saunders <lukesaunders@Sams-MacBook-Pro.local>
2023-09-23 15:51:40 +0100

    feat: fractional decimal moneyline helpers

--- src/index.ts
+++ src/index.ts
@@ -10,6 +10,11 @@ class BetBrewClass {
         }
     }
 
+    private validateString(input: string, name: string): void {
+        if (typeof input !== 'string' || input.trim() === '') {
+            throw new TypeError(`${name} must be a valid non-empty string.`);
+        }
+    }
     private validateArray(input: any[], name: string): void {
         if (!Array.isArray(input)) {
             throw new TypeError(`${name} must be an array.`);
@@ -63,6 +68,37 @@ class BetBrewClass {
     public calculateAdjustedROI(input: AdjustedROIInput): number {
         const adjustedEv: number = this.calculateAdjustedEV(input);
         return (adjustedEv / input.stakedAmount) * 100;
+    }
+
+    public decimalToFractional(decimalOdds: number): string {
+        this.validateNumber(decimalOdds, 'decimalOdds', true);
+        const numerator = decimalOdds - 1;
+        const denominator = 1;
+        return `${numerator}/${denominator}`;
+    }
+
+    public fractionalToDecimal(fraction: string): number {
+        this.validateString(fraction, 'fractional odds');
+        const [numerator, denominator] = fraction.split('/').map(Number);
+        return numerator / denominator + 1;
+    }
+
+    public decimalToMoneyline(decimalOdds: number): number {
+        this.validateNumber(decimalOdds, 'decimalOdds', true);
+        if (decimalOdds >= 2) {
+            return +(decimalOdds - 1) * 100;
+        } else {
+            return -100 / (decimalOdds - 1);
+        }
+    }
+
+    public moneylineToDecimal(moneyline: number): number {
+        this.validateNumber(moneyline, 'moneyline');
+        if (moneyline > 0) {
+            return moneyline / 100 + 1;
+        } else {
+            return 1 - 100 / moneyline;
+        }
     }
 }
 
